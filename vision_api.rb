63c968be8a31393b9baeed8624db34669c9d348c
Masa Jobara <wolf.masa@gmail.com>
2016-07-26 23:46:18 +0900

    initial commit

---
+++ vision_api.rb
@@ -0,0 +1,39 @@
+require 'base64'
+require 'json'
+require 'net/https'
+
+
+IMAGE_FILE = './demo-image.jpg'
+
+API_KEY = ''
+require './key_file.rb'
+API_URL = "https://vision.googleapis.com/v1/images:annotate?key=#{API_KEY}"
+
+# 画像をbase64にエンコード
+base64_image = Base64.strict_encode64(File.new(IMAGE_FILE, 'rb').read)
+
+# APIリクエスト用のJSONパラメータの組み立て
+body = {
+  requests: [{
+    image: {
+      content: base64_image
+    },
+    features: [
+      {
+        type: 'LABEL_DETECTION',
+        maxResults: 5
+      }
+    ]
+  }]
+}.to_json
+
+# Google Cloud Vision APIにリクエスト投げる
+uri = URI.parse(API_URL)
+https = Net::HTTP.new(uri.host, uri.port)
+https.use_ssl = true
+request = Net::HTTP::Post.new(uri.request_uri)
+request["Content-Type"] = "application/json"
+response = https.request(request, body)
+
+# APIレスポンス出力
+puts response.body
